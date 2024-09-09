e51b7f9c92b5f4f433cc7df6b76f4cf6ee90b59d
Gelvazio <gelvazio@hotmail.com>
2024-09-09 19:14:06 -0300

    ok

---
+++ conexaodb/index.php
@@ -0,0 +1,30 @@
+<?php
+require_once("ControllerApiBase.php");
+class Principal extends ControllerApiBase {
+
+    public function __construct(){
+        $this->listaDados();
+    }
+
+    public function listaDados() {
+        $sql = " select * from sistema";
+        $aDados = $this->getQuery()->selectAll($sql);
+        $xDados = array();
+        foreach ($aDados as $sDados){
+            array_push($xDados, $sDados);
+        }
+
+        $total = $this->getQuery()->select("select count(*) as total from sistema");
+        $total = intval($total["total"]);
+        
+        $aDados = array(
+            "success" => true,
+            "total" => $total,
+            "sistema" => $xDados
+        );
+        
+        echo "<pre>" . print_r($aDados, true) ."</pre>";
+    }
+}
+
+new Principal();
